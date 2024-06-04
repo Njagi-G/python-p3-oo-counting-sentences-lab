87f3db86b6874624b5d74d5e3312b5be57b3df2f
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-04 16:51:49 +0300

    Add methods to identify a string that is a plain sentence, interrogative sentence, exclamative & method to count sentences.

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,77 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    pass
+    def __init__(self, value = ""):
+        self.value = value
+    
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if type(value) == str:
+            self._value = value
+        else:
+            print("The value must be a string.")
+                      
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        value = self.value
+        for punctuation in ["!", "?"]:
+            value = value.replace(punctuation, ".")
+
+        sentences = [sentence for sentence in value.split(".") if sentence]
+
+        return len(sentences)
+
+
+string = MyString()
+
+string.value = "This is a string!"
+print(string.is_sentence())
+print(string.is_question())
+print(string.is_exclamation())
+print(string.count_sentences())
+
+setattr(string, "value", "It has three sentences.")
+print(string.is_sentence())
+print(string.is_question())
+print(string.is_exclamation())
+print(string.count_sentences())
+
+setattr(string, "value", "Right?")
+print(string.is_sentence())
+print(string.is_question())
+print(string.is_exclamation())
+print(string.count_sentences())
+
+setattr(string, "value", "This is a string! It has three sentences. Right?")
+print(string.is_sentence())
+print(string.is_question())
+print(string.is_exclamation())
+print(string.count_sentences())
+
+new_string = MyString("Hello.")
+print(new_string.value)
+
+new_string.value = 234
+print(new_string.value)
+
+
+    
+
+
+
+
+
+
